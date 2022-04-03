be812d0c457b49fe644cc21b5b5f2a581a125b1c
pyvvo <kenneth.ag@humaapi.com>
2022-04-03 19:58:54 +0200

    feat(cli): cli migration

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "rollup";
-// import json from "@rollup/plugin-json";
+import json from "@rollup/plugin-json";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import cjs from "@rollup/plugin-commonjs";
 import esbuild from "rollup-plugin-esbuild";
-import shebang from 'rollup-plugin-preserve-shebang';
+import shebang from "rollup-plugin-preserve-shebang";
+import alias from "@rollup/plugin-alias";
+import path from "path";
+
+const customResolver = nodeResolve({
+  extensions: [".ts", ".js", ".json", "/index.ts"]
+});
+const projectRootDir = path.resolve(__dirname);
+console.log("path", path.resolve(projectRootDir, "src/utils"));
 
 const rollupConfig = defineConfig([
   {
@@ -13,13 +21,24 @@ const rollupConfig = defineConfig([
       format: "commonjs"
     },
     plugins: [
+      alias({
+        customResolver,
+        entries: [
+          {
+            find: "@/utils",
+            replacement: path.resolve(projectRootDir, "./src/utils")
+            // OR place `customResolver` here. See explanation below.
+          }
+        ]
+      }),
       esbuild({
         // All options are optional
         // include: [
         //   "src/**/*.js",
         // ],
         sourceMap: false,
-        exclude: ["node_modules"],
+        include: ["src/**/*.ts"],
+        // exclude: ["node_modules"],
         minify: true,
         target: "node14.13.0", // default, or 'es20XX', 'esnext'
         tsconfig: "tsconfig.json",
@@ -29,8 +48,11 @@ const rollupConfig = defineConfig([
           // '.js': 'jsx',
         }
       }),
-      cjs(),
-      shebang({ shebang: '#!/usr/bin/env node' }),
+      json(),
+      cjs({
+        requireReturnsDefault: "auto"
+      }),
+      shebang({ shebang: "#!/usr/bin/env node" }),
       nodeResolve({
         preferBuiltins: true,
         exportConditions: ["node"] // https://github.com/uuidjs/uuid/issues/544#issuecomment-740394448
